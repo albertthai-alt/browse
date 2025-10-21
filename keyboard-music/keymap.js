e004bd189184f852f0c847a794893e95d5927b59
albertthai-alt <albertthai@gmail.com>
2025-10-22 06:06:29 +0700

    keyboard music

---
+++ keyboard-music/keymap.js
@@ -0,0 +1,10 @@
+// Shared keyboard-to-note mapping for all instruments
+// Exposes window.SHARED_KEYMAP
+(function(){
+  window.SHARED_KEYMAP = {
+    q:'C3', w:'D3', e:'E3', r:'F3', t:'G3', y:'A3', u:'B3',
+    i:'C4', o:'D4', p:'E4', a:'F4', s:'G4', d:'A4', f:'B4',
+    g:'C5', h:'D5', j:'E5', k:'F5', l:'G5',
+    z:'A5', x:'B5', c:'C6', v:'D6', b:'E6', n:'F6', m:'G6', ',':'A6'
+  };
+})();
