9ed92a8389be5f773b472cf83a5f6430a48647e9
albertthai-alt <albertthai@gmail.com>
2025-10-24 09:55:32 +0700

    keyboard music

--- keyboard-music/keymap.js
+++ keyboard-music/keymap.js
@@ -2,9 +2,12 @@
 // Exposes window.SHARED_KEYMAP
 (function(){
   window.SHARED_KEYMAP = {
-    q:'C3', w:'D3', e:'E3', r:'F3', t:'G3', y:'A3', u:'B3',
-    i:'C4', o:'D4', p:'E4', a:'F4', s:'G4', d:'A4', f:'B4',
-    g:'C5', h:'D5', j:'E5', k:'F5', l:'G5',
-    z:'A5', x:'B5', c:'C6', v:'D6', b:'E6', n:'F6', m:'G6', ',':'A6'
+    // Top row (only sharps on selected keys)
+    w:'F#3', e:'G#3', r:'A#3', y:'C#4', u:'D#4', o:'F#4', p:'G#4',
+    // Middle row
+    a:'F3', s:'G3', d:'A3', f:'B3',
+    g:'C4', h:'D4', j:'E4', k:'F4', l:'G4',
+    // Bottom row
+    z:'B2', x:'C3', c:'D3', v:'E3', b:'A4', n:'B4', m:'C5', ',':'D5', '.':'E5'
   };
 })();
