49f16181b626722e626d0332df879a1dba4db7a6
albertthai-alt <albertthai@gmail.com>
2025-10-25 23:49:03 +0700

    keyboard music

--- keyboard-music/keymap.js
+++ keyboard-music/keymap.js
@@ -4,6 +4,8 @@
   window.SHARED_KEYMAP = {
     // Top row sharps (others intentionally omitted)
     w:'C#4', e:'D#4', t:'F#4', y:'G#4', u:'A#4', o:'C#5', p:'D#5',
+    // Extra naturals on top row
+    q:'C6', r:'D6', i:'F6',
     // Natural keys in order ZXCVASDFGHJKLBNM,. mapped F3..B5
     z:'F3', x:'G3', c:'A3', v:'B3',
     a:'C4', s:'D4', d:'E4', f:'F4', g:'G4', h:'A4', j:'B4', k:'C5', l:'D5',
